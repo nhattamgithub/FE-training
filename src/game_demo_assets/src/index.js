4bcb12ec6a6d786145013b4b11c46f57fe6e85f7
nhattamgithub <hiimhyouka@gmail.com>
2022-06-21 15:40:16 +0700

    Initial commit.

---
+++ src/game_demo_assets/src/index.js
@@ -0,0 +1,19 @@
+import { game_demo } from "../../declarations/game_demo";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await game_demo.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
